894bc7ac563f065c4b214c7ce917f5d99a759783
viscoadam5 <122196288+viscoadam5@users.noreply.github.com>
2023-03-14 12:39:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,61 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Enkripsi Dan Dekripsi APPS</title>
+</head>
+<body bgcolor="#32a887">
+    <center>
+        <h2>
+            <u><b>KEY13 Algoritma Caesar Cipher</b></u>
+        </h2>
+        <h3>
+            Enkripsi Dan Dekripsi
+        </h3>
+        <form action="" method="get">
+            Kalimat : <input type="text" name="kalimat">
+            <br><br>
+            <input type="submit" value="kirim">
+            <input type="reset" value="reset">
+            <br><br>
+        </form>
+    </center>
+    <?php
+        $msg = $_GET['kalimat'];
+        $key = 13;
+        $mode = 'decrypt';
+        $letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $trans = '';
+        $msg = strtoupper($msg);
+        
+        for ($i = 0; $i < strlen($msg); $i++) {
+            if (strpos($letters, $msg[$i]) !== false) {
+                $num = strpos($letters, $msg[$i]);
+            if ($mode == 'encrypt') {
+                $num += $key;
+            } else {
+                $num -= $key;
+            }
+            $cnt = strlen($letters);
+            if ($num >= $cnt) {
+                $num -= $cnt;
+            } else {
+                $num += $cnt;
+            }
+            if ($num >= $cnt) {
+                $num = $num - $cnt;
+            }
+            $trans .= $letters[$num];
+            }
+        }
+    ?>
+    <center>
+        <?php
+            echo 'Hasil : ';
+            echo $trans . PHP_EOL;
+        ?>
+    </center>
+</body>
+</html>
